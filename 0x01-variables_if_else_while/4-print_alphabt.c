a37d6b14a45be64d165663c99f291b9f1f587702
salisu shamsuddeen <111056873+Totudansabo@users.noreply.github.com>
2022-09-09 23:27:28 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
 /**
- *   * main - Prints some letters of alphabet
- *     *
- *       * Return: Always (Success)
- *         */
+  * main - Prints some letters of alphabet
+  *
+  * Return: Always (Success)
+  */
 int main(void)
 {
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
 	{
-			if (c != 'e' && c != 'q')
-			{
-						putchar(c);
-					}
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
 		}
+	}
 
 	putchar('\n');
 
 	return (0);
-}}
+}
