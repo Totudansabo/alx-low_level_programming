5cb030ea4c46c76e5a8a0f6ac8064157ab3dd687
Totudansabo <salisushamsuddeen7@gmail.com>
2022-09-15 08:35:45 -0700

    I feel like I am diagonally parked in a parallel universe

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,24 +1,23 @@
 #include "main.h"
 
-/*
- * more_numbers -  checks for checks for a digit (0 through 9).
- * Return: Always 0.
+/**
+ * more_numbers - nmbr fct
+ *
+ * Return: no return
  */
 
 void more_numbers(void)
 {
-	int a, ro;
 
-	for (ro = 0; ro < 10; ro++)
+int i, j;
+	for (i = 1; i <= 10; i++)
 	{
-		for (a = 0; a <= 14; a++)
+		for (j = 0; j < 15; j++)
 		{
-			if (a >= 10)
-			{
-				_putchar((a / 10) + '0');
-			}
-			_putchar((a % 10) + '0');
+			if (j >= 10)
+			_putchar('1');
+			_putchar(j % 10 + '0');
 		}
-		_putchar('\n');
-	}
+			_putchar('\n');
+		}
 }
