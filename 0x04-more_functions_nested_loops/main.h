0408476772c6d141dface209f5985180e45b0ca0
Totudansabo <salisushamsuddeen7@gmail.com>
2022-09-15 09:32:27 -0700

    The problem of distinguishing prime numbers from composite numbers and of resolving the latter into their prime factors is known to be one of the most important and useful in arithmetic

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -13,5 +13,6 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+unsigned int long n = 612852475143, a = (int) sqrt(n);
 
 #endif /* MAIN_H */
