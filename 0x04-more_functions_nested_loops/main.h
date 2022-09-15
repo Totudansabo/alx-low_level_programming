78a2cf33034e7b193af9134ccb6e1ca8b7afd804
Totudansabo <salisushamsuddeen7@gmail.com>
2022-09-15 09:54:15 -0700

    ttriangle

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -13,6 +10,3 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-unsigned int long n = 612852475143, a = (int) sqrt(n);
-
-#endif /* MAIN_H */
